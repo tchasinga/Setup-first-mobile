e34f1491f43bca05433e7ddde15589013e6f8d34
Balolebwami <tchasingajacques@gmail.com>
2023-05-22 13:27:25 +0200

    new pushe

---
+++ CodeScript/Runner.js
@@ -0,0 +1,14 @@
+const menuHamburger = document.querySelector(".menu-humberger")
+const navLinks = document.querySelector(".nav-links")
+const closeTage = document.querySelector(".closeHumberger");
+
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+menuHamburger.style.display = "none";
+})
+
+closeTage.addEventListener('click',()=>{
+navLinks.classList.remove('mobile-menu')
+menuHamburger.style.display = "block";
+})
+
