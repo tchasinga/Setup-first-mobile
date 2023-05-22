ab7a9223497700f8e1cc23b8b31b855ca796e879
zerihun abune <zerihunabune@gmail.com>
2023-05-22 15:15:04 +0300

    Added an event listener to the navigation

--- CodeScript/Runner.js
+++ CodeScript/Runner.js
@@ -1,8 +1,13 @@
 const menuHamburger = document.querySelector('.menu-humberger');
 const navLinks = document.querySelector('.nav-links');
-// const closeTage = document.querySelector('.closeHumberger');
+const closeTage = document.querySelector('.closeHumberger');
 
 menuHamburger.addEventListener('click', () => {
   navLinks.classList.toggle('mobile-menu');
   menuHamburger.style.display = 'none';
 });
+
+closeTage.addEventListener('click',()=>{
+  navLinks.classList.remove('mobile-menu')
+  menuHamburger.style.display = "block";
+})
