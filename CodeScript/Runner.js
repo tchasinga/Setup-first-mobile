df3feb0a921387136db9af9e32cf104f7c2031c4
Balolebwami <tchasingajacques@gmail.com>
2023-05-22 13:57:48 +0200

    new code testing

--- CodeScript/Runner.js
+++ CodeScript/Runner.js
@@ -1,14 +1,8 @@
-const menuHamburger = document.querySelector(".menu-humberger")
-const navLinks = document.querySelector(".nav-links")
-const closeTage = document.querySelector(".closeHumberger");
+const menuHamburger = document.querySelector('.menu-humberger');
+const navLinks = document.querySelector('.nav-links');
+// const closeTage = document.querySelector('.closeHumberger');
 
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
-menuHamburger.style.display = "none";
-})
-
-closeTage.addEventListener('click',()=>{
-navLinks.classList.remove('mobile-menu')
-menuHamburger.style.display = "block";
-})
-
+menuHamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('mobile-menu');
+  menuHamburger.style.display = 'none';
+});
