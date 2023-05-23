0820456fffbc7748a217df3070bf321c49c4c93d
Balolebwami <tchasingajacques@gmail.com>
2023-05-24 00:46:01 +0200

    new checking

--- javascript/generate-html-utils.js
+++ javascript/generate-html-utils.js
@@ -22,53 +22,59 @@ export const generateProjectPopUps = (projectDetails) => {
   } = projectDetails;
 
   return `
-      <div class="containinger">
-        <div id="modal-card" class="card">
-          <div class="card-body">
-            <div class="card-header"> 
-              <h2>${name}</h2>
-              <i id="close-modal-popup" class="fas fa-times"></i>
-            </div>
-            <ul class="details">
-              <li class="Newrole">
-                <a href="#">${role}<i class="fa fa-circle"></i></a>
-              </li>
-              <li class="Newrole">
-                <a href="#">${platform}<i class="fa fa-circle"></i></a>
-              </li>
-              <li class="Newrole"><a href="#">${year}</a></li>
-            </ul>
-            <img src="${featuredImage}" class="PopUpImg" alt="" />
-          </div>
-          <div class="griding">
-            <p>
-                ${descriptionlong}
-            </p>
-            <div class="btnn">
-              <ul class="tags">
-              <li>
-              <a href="#" class="tagion">${techOne}</a>
-              </li>
+  <div class="keeper">
+  <div class="oneDiver">
+    <h2>${name}</h2>
+    <i id="close-modal-popup" class="fas fa-times"></i>
+  </div>
+<!--First side done-->
+  <div class="twoDiver">
+    <ul class="details">
+      <li class="Newrole">
+        <a href="#">${role}<i class="fa fa-circle"></i></a>
+      </li>
+      <li class="Newrole">
+        <a href="#">${platform}<i class="fa fa-circle"></i></a>
+      </li>
+      <li class="Newrole"><a href="#">${year}</a></li>
+    </ul>
+  </div>
+<!--Second side done-->
+  <div class="threeDvier">
+    <img src="${featuredImage}" class="PopUpImg" alt="" />
+  </div>
+<!--Third side done-->
+  <div class="fourDvier">
+    <div class="griding">
+      <p>
+        ${descriptionlong}
+      </p>
+    </div>
+  <div class="btnn">
+      <ul class="tags">
+        <li>
+        <a href="#" class="tagion">${techOne}</a>
+        </li>
 
-              <li>
-              <a href="#" class="taging">${techTwo}</a>
-              </li>
+        <li>
+        <a href="#" class="taging">${techTwo}</a>
+        </li>
 
-              <li >
-              <a href="#" class="tagiono">${techThree}</a>
-              </li>
-              </ul>
-              <div class="btn-group">
-                <a href="${linkToLiveVersion}" class="btn btn-outline">
-                  See live <img src="Svg/LinksIcon.svg" alt="">
-                </a>
-                <a href="${linkToSource}" class="btn btn-outline">
-                  See source <img src="IconGitHubi.svg" alt="">
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        <li >
+        <a href="#" class="tagiono">${techThree}</a>
+        </li>
+        </ul>
+
+   <div class="btn-group">
+     <a href="${linkToLiveVersion}" class="btn btn-outline">
+       See live <img src="Svg/LinksIcon.svg" alt="">
+     </a>
+     <a href="${linkToSource}" class="btn btn-outline">
+       See source <img src="IconGitHubi.svg" alt="">
+     </a>
+   </div>
+  </div>
+</div>
+</div>
   `;
 };
